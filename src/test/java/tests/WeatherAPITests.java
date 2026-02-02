9c58662d19a9a3252811f375cfbdf2391a1b9139
Buisiwe <buisiwemad@gmail.com>
2026-02-02 20:16:58 +0200

    added test to WeatherAPITest from priority 1-5

--- src/test/java/tests/WeatherAPITests.java
+++ src/test/java/tests/WeatherAPITests.java
@@ -12,12 +12,13 @@ import java.io.File;
 import static common.BaseURI.baseURL;
 import static payloadBuilder.TestimonialsPayload.loginUserPayload;
 import static payloadBuilder.WeatherPayload.registerStationPayload;
+import static payloadBuilder.WeatherPayload.sendMeasurementPayload;
 
 public class WeatherAPITests {
     static String ap;
 
 
-    @Test
+    @Test (priority = 1)
     public static Response registerStation(String external_id, String name, double latitude, double longitude, int altitude) {
         Response response = RestAssured.given()
                 .baseUri(baseURL)
@@ -39,4 +40,115 @@ public class WeatherAPITests {
         }
 
         return response;
-}}
+}
+@Test (priority = 2)
+public static Response sendMeasurement(double temperature, double windSpeed, int humidity, String dateUTC) {
+        Response response = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath("/measurements")
+                .queryParam("appid", ap)
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(sendMeasurementPayload(temperature, windSpeed, humidity, dateUTC))
+                .post()
+                .then()
+                .extract().response();
+
+        int status = response.getStatusCode();
+        if (status == 200 || status == 201) {
+            response.then().assertThat()
+                    .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/schemas/sendMeasurement.json")));
+        } else {
+            Assert.fail("API error: HTTP " + status + " - " + response.asString());
+        }
+
+        return response;
+    }
+    @Test (priority = 3)
+    public static Response getStations() {
+        Response response = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath("/stations")
+                .queryParam("appid", ap)
+                .contentType(ContentType.JSON)
+                .log().all()
+                .get()
+                .then()
+                .extract().response();
+
+        int status = response.getStatusCode();
+        if (status == 200) {
+            response.then().assertThat()
+                    .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/schemas/getStations.json")));
+        } else {
+            Assert.fail("API error: HTTP " + status + " - " + response.asString());
+        }
+
+        return response;
+    }
+    @Test (priority = 4)
+    public static Response getStationID () {
+        Response response = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath("/stations/{station_id}")
+                .queryParam("appid", ap)
+                .contentType(ContentType.JSON)
+                .log().all()
+                .get()
+                .then()
+                .extract().response();
+
+        int status = response.getStatusCode();
+        if (status == 200) {
+            response.then().assertThat()
+                    .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/schemas/getStationById.json")));
+        } else {
+            Assert.fail("API error: HTTP " + status + " - " + response.asString());
+        }
+
+        return response;
+    }
+    @Test (priority = 5)
+    public static Response updateStation(String external_id, String name, double latitude, double longitude, int altitude) {
+        Response response = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath("/stations/{station_id}")
+                .queryParam("appid", ap)
+                .contentType(ContentType.JSON)
+                .log().all()
+                .body(registerStationPayload(external_id, name, latitude, longitude, altitude))
+                .put()
+                .then()
+                .extract().response();
+
+        int status = response.getStatusCode();
+        if (status == 200) {
+            response.then().assertThat()
+                    .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/schemas/updateStation.json")));
+        } else {
+            Assert.fail("API error: HTTP " + status + " - " + response.asString());
+        }
+
+        return response;
+    }
+    @Test (priority = 6)
+    public static Response deleteStation() {
+        Response response = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath("/stations/{station_id}")
+                .queryParam("appid", ap)
+                .contentType(ContentType.JSON)
+                .log().all()
+                .delete()
+                .then()
+                .extract().response();
+        int status = response.getStatusCode();
+        if (status == 204) {
+            response.then().assertThat()
+                    .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/java/schemas/deleteStation.json")));
+        } else {
+            Assert.fail("API error: HTTP " + status + " - " + response.asString());
+        }
+        return response;
+    }
+}
